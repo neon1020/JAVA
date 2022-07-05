859b856b19bc790ac0738fcdc43baf059f64062f
neon1020 <neo3111@naver.com>
2022-07-05 23:08:31 +0900

    first java commit

---
+++ JAVA/src/JAVA0705/Ex01.java
@@ -0,0 +1,48 @@
+package JAVA0705;
+
+public class Ex01 {
+public static void main(String[] args) {
+	/*
+	 * 변수 (Variable)
+	 * - 자바에서 사용되는 데이터를 저장하는 메모리 공간
+	 * - 한 번에 하나의 데이터만 저장 가능
+	 *   => 언제든 다른 데이터로 대체 가능 (변할 수 있다 = 변수)
+	 * - 변수를 사용하기 위해선 변수 선언이 먼저 진행되어야 함
+	 * - 변수에 데이터를 저장하는 것을 변수 "초기화"라고함  
+	 * 
+	 * */
+	
+//	int a;
+//	int b;
+//	a = 1;
+//	System.out.println(a + 1);
+//	System.out.println(b); // 반드시 초기화 후에 사용해야함
+//	a = 2;
+//	System.out.println(a + 1);
+	
+	// 선언과 초기화를 동시에
+//	int a = 10;
+//	int b = 20;
+	
+	// 동시에 여러개 변수를 선언
+//	int a, b;
+//	a = 10;
+//	b = 20;
+	
+	// 동시에 여러개 변수를 선언과 동시에 초기화
+	// a, b의 값 바꾸기
+	int a = 10, b = 20, c = 0;
+	c = a;
+	a = b;
+	b = c;
+	
+	System.out.println("a = " + a);
+	System.out.println("b = " + b);
+	
+	String name;
+	name = "정채연";
+	System.out.println(name);
+	
+	
+}
+}
---
+++ JAVA/src/JAVA0705/Ex02.java
@@ -0,0 +1,30 @@
+package JAVA0705;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		/*
+		 * 상수 (Constant)
+		 * - 변수의 반대 개념으로, 항상 고정된 데이터 (변하지 않는 데이터)
+		 * - 실제 사용하는 데이터(상수)를 리터럴(Literal) 이라고도 한다.
+		 *   ex) 정수형 리터럴 1, 실수형 리터럴 3.14 등
+		 * 
+		 * */
+		
+		System.out.println(1);	// 정수형(기본형 = int형) 리터럴
+		System.out.println(3.14); // 실수형(기본형 = double) 리터럴
+		
+		System.out.println('A'); // 문자형 리터럴(작은따옴표로 둘러싼 1개의 문자)
+//		System.out.println('AB'); // 오류! 문자형은 1개의 문자만 입력할 수 있다!
+		
+		System.out.println(true); // 논리형(boolean형) 리터럴(true 또는 false)
+//		System.out.println(TRUE); // 오류! 예약어. true는 무조건 소문자로 써야함!
+		
+		System.out.println(100L); // 정수형(long형) 리터럴(접미사 L(구분하기 위함) 붙는다.)
+		System.out.println(1.5f); // 실수형(float형) 리터럴 (접미사 f 또는 F 붙는다.)
+		System.out.println("Hello, World!"); // 문자열형 리터럴 (큰 따옴표로 둘러싼 문자들)
+		System.out.println("");
+//		System.out.println(''); // 오류! 문자형에는 "무조건" 1개의 문자를 적어줘야함!!
+		
+	}
+
+}
